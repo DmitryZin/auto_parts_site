de036343a3173e8103849dc849ed1777ea016b03
dmitryzin <dmitryzin@gmail.com>
2023-02-14 01:04:34 +0300

    init project

---
+++ src/main/java/com/gmail/dmitryzin/auto_parts_site/AutoPartsSiteApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.dmitryzin.auto_parts_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPartsSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPartsSiteApplication.class, args);
+	}
+
+}
