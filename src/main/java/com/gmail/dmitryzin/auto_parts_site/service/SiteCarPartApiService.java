1d9dd6b2026d219f760673e4fa76422fb3a4933d
dmitryzin <dmitryzin@gmail.com>
2023-02-16 02:57:03 +0300

    +NotFoundException

--- src/main/java/com/gmail/dmitryzin/auto_parts_site/service/SiteCarPartApiService.java
+++ src/main/java/com/gmail/dmitryzin/auto_parts_site/service/SiteCarPartApiService.java
@@ -3,6 +3,7 @@ package com.gmail.dmitryzin.auto_parts_site.service;
 import com.gmail.dmitryzin.auto_parts_site.dto.CarPartDto;
 import com.gmail.dmitryzin.auto_parts_site.dto.Paginator;
 import com.gmail.dmitryzin.auto_parts_site.dto.PreviewCarPartDto;
+import com.gmail.dmitryzin.auto_parts_site.exception.NotFoundException;
 import com.gmail.dmitryzin.auto_parts_site.mapper.PreviewCarPartMapper;
 import com.gmail.dmitryzin.auto_parts_site.repository.CarPartRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,41 +20,41 @@ public class SiteCarPartApiService implements SiteApiService{
     private final CarPartRepository carPartRepository;
     @Override
     public CarPartDto getCarPart(Integer part_id) {
-        return carPartRepository.getCarPartById(part_id).orElseThrow();
+        return carPartRepository.getCarPartById(part_id).orElseThrow(NotFoundException::new);
     }
 
     @Override
     public List<PreviewCarPartDto> getPreviewCarPartList(Paginator paginator) {
-        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartList(paginator.getPageNum(), paginator.getPageCount()).orElseThrow());
+        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartList(paginator.getPageNum(), paginator.getPageCount()).orElseThrow(NotFoundException::new));
     }
 
     @Override
     public List<PreviewCarPartDto> getPreviewCarPartListByNumber(String number, Paginator paginator) {
-        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByNumber(paginator.getPageNum(), paginator.getPageCount(), number).orElseThrow());
+        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByNumber(paginator.getPageNum(), paginator.getPageCount(), number).orElseThrow(NotFoundException::new));
     }
 
     @Override
     public List<PreviewCarPartDto> getPreviewCarPartListForAutoByBrand(Integer brand_id, Paginator paginator) {
-        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByBrand(paginator.getPageNum(), paginator.getPageCount(), brand_id).orElseThrow());
+        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByBrand(paginator.getPageNum(), paginator.getPageCount(), brand_id).orElseThrow(NotFoundException::new));
     }
 
     @Override
     public List<PreviewCarPartDto> getPreviewCarPartListForAutoByModel(Integer model_id, Paginator paginator) {
-        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByModel(paginator.getPageNum(), paginator.getPageCount(), model_id).orElseThrow());
+        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByModel(paginator.getPageNum(), paginator.getPageCount(), model_id).orElseThrow(NotFoundException::new));
     }
 
     @Override
     public List<PreviewCarPartDto> getPreviewCarPartListForAutoByCar(Integer car_id, Paginator paginator) {
-        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByCar(paginator.getPageNum(), paginator.getPageCount(), car_id).orElseThrow());
+        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByCar(paginator.getPageNum(), paginator.getPageCount(), car_id).orElseThrow(NotFoundException::new));
     }
 
     @Override
     public List<PreviewCarPartDto> getPreviewCarPartListForAutoByEngine(Integer engine_id, Paginator paginator) {
-        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByEngine(paginator.getPageNum(), paginator.getPageCount(), engine_id).orElseThrow());
+        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartListByEngine(paginator.getPageNum(), paginator.getPageCount(), engine_id).orElseThrow(NotFoundException::new));
     }
 
     @Override
     public List<PreviewCarPartDto> getPreviewCarPartAnalogList(Paginator paginator, Integer carPartId){
-        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartAnalogList(paginator.getPageNum(), paginator.getPageCount(), carPartId).orElseThrow());
+        return PreviewCarPartMapper.mapPreviewCarPartList(carPartRepository.getCarPartAnalogList(paginator.getPageNum(), paginator.getPageCount(), carPartId).orElseThrow(NotFoundException::new));
     };
 }
